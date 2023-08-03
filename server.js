2db269066209c67d4fd538ebbab57dbf215ae045
Sinchana <sinchanashegde06@gmail.com>
2023-08-03 17:27:54 +0530

    changes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/mydb', {
+mongoose.connect('mongodb://127.0.0.1:27017/mydb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
